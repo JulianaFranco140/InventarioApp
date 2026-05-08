623983a3d605b7d994d96fd7da7df35c67d526eb
Juliana Franco <jfranco140@soyudemedellin.edu.co>
2026-05-08 17:36:44 -0500

    Nueva carpeta models

--- Program.cs
+++ Program.cs
@@ -24,4 +24,19 @@ Console.WriteLine("Carpeta /src creada");
 Console.WriteLine("Metadatos configurados");
 Console.WriteLine();
 Console.WriteLine("Próximo paso: Agregar argumnetos CLI");
+Console.WriteLine();
+Console.WriteLine("Estrcutura del proyecto:");
+Console.WriteLine("     /InventarioApp");
+Console.WriteLine("     |--Program.cs");
+Console.WriteLine("     |--InventarioApp.csproj");
+Console.WriteLine("     |--.gitignore");
+Console.WriteLine("     |--README.md");
+Console.WriteLine("     |--src/");
+Console.WriteLine("         |--models/");
+Console.WriteLine("             |--.gitkeep");
+Console.WriteLine("Próximo paso: Checkpoint")
+
+
+
+
 
